f611977a9a00680a75760ef5ea17959e218763ba
Эльдар Корнеев <eldarkorneev@MacBook-Air-Eldar.local>
2022-07-21 22:23:41 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,12 +34,12 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {order: {address, sum}, client: {name, phone}, goods:[]};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i ++) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
     data.order.address = address;
@@ -47,7 +47,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
